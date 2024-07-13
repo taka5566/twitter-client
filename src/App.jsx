3bddaa8bab7480732d1ac5593406f387d1919231
taka5566 <139129841+taka5566@users.noreply.github.com>
2024-07-13 18:50:32 +0800

    rewind

--- src/App.jsx
+++ src/App.jsx
@@ -11,33 +11,33 @@ import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
-  // const {data:authUser, isLoading } = useQuery({
-  //   queryKey:["authUser"],
-  //   queryFn: async() => {
-  //     try {
-  //        const res = await fetch("/api/auth/me")
-  //        const data = await res.json()
-  //        if(data.error) return null
-  //        if(!res.ok) {
-  //         throw new Error(data.error || 'Something went wrong')
-  //        }
-  //        console.log('authUser is here:', data)
-  //        return data
-  //     }catch (error) {
-  //       throw new Error(error)
+  const {data:authUser, isLoading } = useQuery({
+    queryKey:["authUser"],
+    queryFn: async() => {
+      try {
+         const res = await fetch("/api/auth/me")
+         const data = await res.json()
+         if(data.error) return null
+         if(!res.ok) {
+          throw new Error(data.error || 'Something went wrong')
+         }
+         console.log('authUser is here:', data)
+         return data
+      }catch (error) {
+        throw new Error(error)
 
-  //     }
-  //   },
-  //   retry: false,
-  // })
+      }
+    },
+    retry: false,
+  })
 
-  // if(isLoading) {
-  //   return (
-  //     <div className="h-screen flex justify-center items-center">
-  //        <LoadingSpinner size = 'lg' />
-  //     </div>
-  //   )
-  // }
+  if(isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+         <LoadingSpinner size = 'lg' />
+      </div>
+    )
+  }
 
   return (
     <div className='flex max-w-6xl mx-auto'>
